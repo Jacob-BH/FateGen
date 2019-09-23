e79573cb25aa3267be283522bbcc924d46341697
Dex-Sinister <dexter-sinister@hotmail.com>
2019-09-23 21:35:26 +1000

    NPC selector styling
    Modified the NPC selector to be more easily included in a file, and to give the block of names a "table row and cells" styling.

--- php/fate_npcselector.php
+++ php/fate_npcselector.php
@@ -2,11 +2,16 @@
 	require_once 'fateinclude.php';
 	
 	//Selector of NPCs.
-	echo "<form method='post' action='#' id='npcs'>\n";
-	echo "\t<select name='getnpcs[]' multiple='multiple' size='20'>\n";
+	echo "<form method='post' action='#' class='npc_selector'>\n";
+	echo "\t<select name='getnpcs[]' multiple='multiple' size='5'>\n";
+	$npci = 0;
+	echo "\t<optgroup>\n";
 	foreach ($npcs as $npc) {
 		echo "\t\t<option value='$npc'>$npc</option>\n";
+		$npci++;
+		if ( ($npci % 10) == 0) { echo "</optgroup>\n\t<optgroup>\n"; }
 	}
+	echo "\t</optgroup>\n";
 	echo "\t<input type='submit' value='Load'>\n";
 	echo "\n\t</select>\n</form>";
 
